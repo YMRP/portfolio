c3a86a50e53701578de705b326ed5c44c1c1169c
Yostin Manuel Ramos Pinto <ramosyostin2003@gmail.com>
2025-07-18 23:10:31 -0600

    links corregidos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FaReact } from "react-icons/fa";
 function Footer() {
   return (
     <footer className="bottom-0 bg-black p-10 text-white flex items-center gap-5">
-        <p>This page was made by Yostin Ramos, using</p>
+        <p>This page was made using</p>
         <RiTailwindCssFill size={30}/>
         <FaReact size={30}/>
 
--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -10,8 +10,8 @@ function Presentation() {
       <div className="flex gap-5 ">
      
         <a href="https://www.linkedin.com/in/yostin-ramos-a54b88324/"> <FaLinkedin  size={30} className="hover:text-blue-400 duration-200"/></a>
-        <a href="https://web.facebook.com/yostin.ramos.3"> <FaInstagramSquare size={30} className="hover:text-pink-400 duration-200"/> </a>
-        <a href="https://www.instagram.com/yostin_sw/"><FaFacebookSquare  size={30} className="hover:text-blue-700 duration-200"/> </a>
+        <a href="https://www.instagram.com/yostin_sw/"> <FaInstagramSquare size={30} className="hover:text-pink-400 duration-200"/> </a>
+        <a href="https://web.facebook.com/yostin.ramos.3"><FaFacebookSquare  size={30} className="hover:text-blue-700 duration-200"/> </a>
         <a href={cv} download='CV_Yostin-Ramos.pdf' className="p-1 border-2 hover:border-amber-400 duration-200"> Download my CV</a>
 
       </div>
