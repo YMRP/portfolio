8893596909e4a4277063e4b6291b3adae318cfcb
Yostin Manuel Ramos Pinto <ramosyostin2003@gmail.com>
2026-03-09 23:58:00 -0600

    Adding coffee blog

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,14 +8,20 @@ import { SiSpringboot } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaDocker } from "react-icons/fa";
 import { FaJava } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
 
+import coffee from '../assets/cafe.jpg'
 import cajapic from "../assets/cajapic.png";
 import tshirtImg from "../assets/tshirt.png";
-import notes from "../assets/gestornotas.jpg"
+import notes from "../assets/gestornotas.jpg";
 
 function Projects() {
   return (
-    <section className="px-4 py-10 bg-black  border-y  text-white  " id="projects">
+    <section
+      className="px-4 py-10 bg-black  border-y  text-white  "
+      id="projects"
+    >
       <h1 className="text-4xl md:text-5xl font-bold text-center mb-10">
         My Projects.
       </h1>
@@ -52,7 +58,26 @@ Collaborated closely with backend developer to integrate robust and encrypted AP
           code="https://github.com/YMRP/proyecto-caja"
         />
 
-        <Project alt="Gestor notas" description="A note management application developed with Spring Boot, Hibernate, MySQL, and JWT for authentication. It implements an MVC architecture using Controllers, Repositories, and Models, enabling comprehensive note management (CRUD) in a secure and organized manner. The application is Docker-ready for deployment, facilitating portability and scalability." href="https://gestor-de-notas.netlify.app" img={notes} title="NOTES MANAGEMENT SYSTEM" code="https://github.com/YMRP/gestor-notas-front" tools={[<FaReact size={30}/>, <SiMysql size={30}/>, <BiLogoTypescript size={30}/>, <SiSpringboot size={30}/>, <FaDocker size={30}/>, <FaJava size={30}/>]} />
+        <Project
+          alt="Gestor notas"
+          description="A note management application developed with Spring Boot, Hibernate, MySQL, and JWT for authentication. It implements an MVC architecture using Controllers, Repositories, and Models, enabling comprehensive note management (CRUD) in a secure and organized manner. The application is Docker-ready for deployment, facilitating portability and scalability."
+          href="https://gestor-de-notas.netlify.app"
+          img={notes}
+          title="NOTES MANAGEMENT SYSTEM"
+          code="https://github.com/YMRP/gestor-notas-front"
+          tools={[
+            <FaReact size={30} />,
+            <SiMysql size={30} />,
+            <BiLogoTypescript size={30} />,
+            <SiSpringboot size={30} />,
+            <FaDocker size={30} />,
+            <FaJava size={30} />,
+          ]}
+        />
+
+        <Project
+          alt="Blog cafe"
+          description="A website developed with HTML and CSS featuring a coffee blog designed to demonstrate principles of responsive design. The site adapts its structure and visual elements to different screen sizes, such as computers, tablets and mobile phones. Includes typical sections of a blog header, articles, images and navigation bar organized with a clean and modern design. " href="https://blogcafeymrp.netlify.app" img={coffee} title="COFFEE BLOG" tools={[<FaCss3Alt size={30}/>, <FaHtml5 size={30}/>]} code="https://github.com/YMRP/blogCafe"/>
       </div>
     </section>
   );
